886f0dcb7aa1329013d814be3388463636acef31
DarthTeddyBear11 <31748706+DarthTeddyBear11@users.noreply.github.com>
2017-09-08 23:09:26 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-/*
+/* i canched this at 23:09
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
