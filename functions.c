a829e712c49079aaa577556654cff94a08a42680
chapooom <31748703+chapooom@users.noreply.github.com>
2017-09-08 23:10:06 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-/* i canched this at 23:09
+/* i canched this at 23:10
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
